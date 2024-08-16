a48ae901560fb914c2a56c85f6bc5baacdfeb1e7
bikashneupane17 <edubikkash@gmail.com>
2024-08-16 18:23:49 +1000

    Webhook route explicitly added in server before body parsing

--- server.js
+++ server.js
@@ -14,7 +14,50 @@ mongoConnect();
 app.use(cors());
 app.use(morgan("dev"));
 
-app.use("/v1/stripe", stripeWebhookRouter);
+// Confirm Payment Success
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    try {
+      const signature = req.headers["stripe-signature"];
+
+      const endpointSecret = process.env.STRIPE_WEBHOOK_ENDPOINT_SECRET;
+
+      let event;
+
+      console.log("rawBody", req.body);
+      try {
+        event = stripe.webhooks.constructEvent(
+          req.body,
+          signature,
+          endpointSecret
+        );
+        console.log("Event: ", event);
+      } catch (err) {
+        res.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+      }
+
+      if (event.type === "payment_intent.succeeded") {
+        const paymentIntent = event.data.object;
+        console.log("PaymentIntent succeeded: ", paymentIntent);
+
+        // Update order status in the database
+        const order = await updateOrder(
+          { paymentIntentId: paymentIntent.id },
+          { status: "Succeeded" }
+        );
+        console.log("Order updated:", order);
+      }
+
+      // Return a 200 response to acknowledge receipt of the event
+      res.send();
+    } catch (error) {
+      res.status(400).send(`Webhook Error: ${error.message}`);
+    }
+  }
+);
 
 app.use(express.json());
 
